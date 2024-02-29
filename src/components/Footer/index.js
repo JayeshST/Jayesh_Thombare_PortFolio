f1ea4eeee047d2781e7784ff47df93ef28a94840
JayeshST <thombarejayesh001@gmail.com>
2024-02-29 13:44:36 +0530

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,129 @@
+import styled from 'styled-components';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Email from '@mui/icons-material/Email';
+import Mobile from '@mui/icons-material/Smartphone';
+
+
+
+import { Bio } from '../../data/constants';
+import { colors } from '@mui/material';
+import { red } from '@mui/material/colors';
+import { Margin } from '@mui/icons-material';
+
+const FooterContainer = styled.div`
+  width: 100%;
+  padding: 2rem 0;
+  display: flex;
+  justify-content: center;
+  //background: linear-gradient(100.26deg, rgba(0, 102, 255, 0.05) 42.33%, rgba(150, 0, 225, 0.05) 127.07%);
+`;
+
+
+const FooterWrapper = styled.footer`
+  width: 100%;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+  align-items: center;
+  padding: 1rem;
+  color: ${({ theme }) => theme.text_primary};
+`;
+
+const Logo = styled.h1`
+  font-weight: 600;
+  font-size: 20px;
+  color: ${({ theme }) => theme.primary};
+`;
+
+const Nav = styled.nav`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 0.5rem;
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+  justify-content: center;
+  @media (max-width: 768px) {
+    flex-wrap: wrap;
+    gap: 1rem;
+    justify-content: center;
+    text-align: center;
+    font-size: 12px;
+  }
+`;
+
+const NavLink = styled.a`
+color: ${({ theme }) => theme.text_primary};
+  text-decoration: none;
+  font-size: 1.2rem;
+  transition: color 0.2s ease-in-out;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+  @media (max-width: 768px) {
+    font-size: 1rem;
+  }
+`;
+
+const SocialMediaIcons = styled.div`
+  display: flex;
+  margin-top: 1rem;
+`;
+
+const SocialMediaIcon = styled.a`
+  display: inline-block;
+  margin: 0 1rem;
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.text_primary};
+  transition: color 0.2s ease-in-out;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+const Copyright = styled.p`
+  margin-top: 1.5rem;
+  font-size: 0.9rem;
+  color: ${({ theme }) => theme.soft2};
+  text-align: center;
+`;
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <FooterWrapper>
+   <h2 style={{border: '1px solid rgba(0,255,0,0.3)'}}>    Get In Touch</h2>
+       <h1 ><Logo > JAYESH THOMBARE</Logo></h1> 
+          <h4>Pune City in <span style={{color:"orange"}}>Maharashtra,</span> India</h4>
+        <h4 style={{color: "Orange"}}>   <a href='mailto:thombarejayesh001@gmail.com' > thombarejayesh001@gmail.com</a></h4>
+           <h4>+917066830222</h4>
+        <Nav>
+          <NavLink href="#about">About</NavLink>
+          <NavLink href="#skills">Skills</NavLink>
+          <NavLink href="#experience">Experience</NavLink>
+          <NavLink href="#projects">Projects</NavLink>
+          <NavLink href="#education">Education</NavLink>
+        </Nav>
+        <SocialMediaIcons>
+          <SocialMediaIcon href={Bio.facebook} target="display"><FacebookIcon /></SocialMediaIcon>
+          <SocialMediaIcon href={Bio.twitter} target="display"><TwitterIcon /></SocialMediaIcon>
+          <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
+          <SocialMediaIcon href={Bio.linkedin} target="display"><LinkedInIcon /></SocialMediaIcon> 
+          <SocialMediaIcon href={Bio.Email} target="display"><Email /></SocialMediaIcon>
+          <SocialMediaIcon href={Bio.Mobile} target="display"><Mobile /></SocialMediaIcon>
+  
+        </SocialMediaIcons>
+        <Copyright>
+          &copy; 2021-2024 <span style={{color: "Orange"}}>  Jayesh Thombare.</span> All rights reserved.
+        </Copyright>
+
+      </FooterWrapper>
+    </FooterContainer>
+  );
+}
+
+export default Footer;
